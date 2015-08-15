a5216e7c1d012d8f2e838d5656e80fe2d22f2c98
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2015-08-15 15:47:38 +0200

    Added Test MVC Plugin

---
+++ TestApps/MVCPlugin1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCPlugin1.MvcApplication" Language="C#" %>
---
+++ TestApps/MVCPlugin1/Verbs/Plugin1Verb.cs
@@ -0,0 +1,39 @@
+﻿
+using System.ComponentModel.Composition;
+using JBSolutions.Common.Web.Contracts.Web;
+
+namespace MVCPlugin1.Verbs
+{
+    /// <summary>
+    /// Provides a navigational blog verb.
+    /// </summary>
+    [Export(typeof(IActionVerb)), ExportMetadata("Category", "Navigation")]
+    public class Plugin1Verb : IActionVerb
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name
+        {
+            get { return "Plugin 1"; }
+        }
+
+        /// <summary>
+        /// Gets the action.
+        /// </summary>
+        public string Action
+        {
+            get { return "Index"; }
+        }
+
+        /// <summary>
+        /// Gets the controller.
+        /// </summary>
+        public string Controller
+        {
+            get { return "Home"; }
+        }
+        #endregion
+    }
+}
