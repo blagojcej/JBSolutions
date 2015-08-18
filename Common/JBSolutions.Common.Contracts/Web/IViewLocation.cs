b2caefd86bb995219dec8a2b57e35cf228cd0e54
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2015-08-18 23:06:54 +0200

    Added Custom View Engine

---
+++ Common/JBSolutions.Common.Contracts/Web/IViewLocation.cs
@@ -0,0 +1,16 @@
+﻿namespace JBSolutions.Common.Contracts.Web
+{
+    /// <summary>
+    /// Defines View Location for each plugin
+    /// </summary>
+    public interface IViewLocation
+    {
+        #region Properties
+        /// <summary>
+        /// Path to views
+        /// </summary>
+        string ViewsPath { get; }
+
+        #endregion Properties
+    }
+}
