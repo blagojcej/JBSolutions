ccddb9eb3c5430710e7749cf1866087ba5c6817e
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2015-10-17 18:13:44 +0200

    Added Master and Home page to Empty MVC ASP.NET

---
+++ TestApps/MVCTestApp_Basic/Views/Shared/_NavigationItems.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MVCTestApp_Basic.Views.Shared
+{
+    public partial class _NavigationItems : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
